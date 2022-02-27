e9e09de2f44e329f40c8cee4ffccb45230f33a0a
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-02-27 16:57:26 -0500

    Refactored with mocha/chai test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,15 @@
 const request = require('request');
-const query = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${query}`, (error, response, body) => {
-if (error) {
-  console.log('error:', error);
- } else {
-  const data = JSON.parse(body);
-  console.log(data[0].description);
- } 
 
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+     } else {
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
+     } 
+    });
+};
+
+module.exports = { fetchBreedDescription };
